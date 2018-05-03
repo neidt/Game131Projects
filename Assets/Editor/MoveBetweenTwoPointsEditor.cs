7e5e932fb34b99a9be5c01f4182e6a821d3b1a8d
Natalie Eidt <neidt@yahoo.com>
2018-05-02 20:31:15 -0500

    Obstacle moves with track

--- Assets/Editor/MoveBetweenTwoPointsEditor.cs
+++ Assets/Editor/MoveBetweenTwoPointsEditor.cs
@@ -7,6 +7,7 @@ public class MoveBetweenTwoPointsEditor : Editor
 {
     //get the script with the variables needed
     MoveBetweenTwoPoints moveScript;
+    Transform obstacle;
     SerializedProperty position;
 
     //private SerializedProperty startPosition;
@@ -15,60 +16,18 @@ public class MoveBetweenTwoPointsEditor : Editor
     public override void OnInspectorGUI()
     {
         moveScript = ((MonoBehaviour)target).gameObject.GetComponent<MoveBetweenTwoPoints>();
-        Event currentEvent = Event.current;
-        
+        obstacle = ((MonoBehaviour)target).gameObject.GetComponent<Transform>();
+
         /*
         startPosition = this.serializedObject.FindProperty("startPosition");
         endPosition = this.serializedObject.FindProperty("endPosition");*/
 
         base.OnInspectorGUI();
+
         //show the start and end positions
-        GUILayout.Label("Start Position: " + moveScript.startPosition);
-        //GUILayout.Label("startPosition" + startPosition);
-        GUILayout.Label("End Position: " + moveScript.endPosition);
-
-        //get position of mouse and set the start point when holding left control
-        switch (currentEvent.type)
-        {
-            case EventType.KeyDown:
-                if (currentEvent.keyCode == KeyCode.LeftAlt)
-                {
-                    UnityEngine.MonoBehaviour.print("Setting Start Position");
-
-                    Vector3 screenPosition = Event.current.mousePosition;
-                    screenPosition.y = Camera.current.pixelHeight - screenPosition.y;
-                    Ray ray = Camera.current.ScreenPointToRay(screenPosition);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit))
-                    {
-                        // do stuff here using hit.point
-                        moveScript.startPosition = hit.point;
-                        // tell the event system you consumed the click
-                        Event.current.Use();
-                    }
-                    
-                }
-                if (currentEvent.keyCode == KeyCode.LeftControl)
-                {
-                    UnityEngine.MonoBehaviour.print("Setting End Position");
-
-                    Vector2 mousePos = currentEvent.mousePosition;
-                    mousePos.y = Camera.current.pixelHeight - mousePos.y;
-                    Vector3 worldPos = Camera.current.ScreenToWorldPoint(mousePos);
-                    worldPos.z = 0;
-                    position.vector3Value = worldPos;
-                    moveScript.startPosition = worldPos;
-
-                    currentEvent.Use();
-                }
-                //currentEvent.Use();
-                break;
-            case EventType.KeyUp:
-                UnityEngine.MonoBehaviour.print("Key Up: " + currentEvent.keyCode);
-                currentEvent.Use();
-                break;
-
-        }
+        GUILayout.Label("Start Position (hold 'a' to change): " + moveScript.startPosition);
+        GUILayout.Label("End Position (hold 'd' to change): " + moveScript.endPosition);
+        GUILayout.Label("Obstacle Position (hold 's' to change): " + obstacle.transform.position);
         this.serializedObject.ApplyModifiedProperties();
     }
     void OnEnable()
@@ -80,39 +39,55 @@ public class MoveBetweenTwoPointsEditor : Editor
     {
         //HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
         moveScript = ((MonoBehaviour)target).gameObject.GetComponent<MoveBetweenTwoPoints>();
+        obstacle = ((MonoBehaviour)target).gameObject.GetComponent<Transform>();
         Event currentEvent = Event.current;
-
-        //show the start and end positions
-        GUILayout.Label("Start Position: (hold a to change) " + moveScript.startPosition);
-        //GUILayout.Label("startPosition" + startPosition);
-        GUILayout.Label("End Position: (hold d to change) " + moveScript.endPosition);
-
+        
         //get position of mouse and set the start point when holding left control
         switch (currentEvent.type)
         {
             case EventType.KeyDown:
                 if (currentEvent.keyCode == KeyCode.A)
                 {
-                    UnityEngine.MonoBehaviour.print("Setting Start Position");
-
-                    Vector3 screenPosition = Event.current.mousePosition;
-                    screenPosition.y = Camera.current.pixelHeight - screenPosition.y;
-                    Vector3 worldpos = Camera.current.ScreenToWorldPoint(screenPosition);
-                    UnityEngine.MonoBehaviour.print("Mouse pos: " + worldpos);
-                    moveScript.startPosition = worldpos;
-                    
-
-                }
-                if (currentEvent.keyCode == KeyCode.D)
-                {
-                    UnityEngine.MonoBehaviour.print("Setting End Position");
+                    //set start position
+                    UnityEngine.MonoBehaviour.print("Setting Track's Start Position");
                     Vector3 screenPosition = Event.current.mousePosition;
                     screenPosition.y = Camera.current.pixelHeight - screenPosition.y;
                     Vector3 worldpos = Camera.current.ScreenToWorldPoint(screenPosition);
                     worldpos.z = 0;
-                    UnityEngine.MonoBehaviour.print("Mouse pos: " + screenPosition);
+                    //UnityEngine.MonoBehaviour.print("Mouse pos: " + worldpos);
+                    moveScript.startPosition = worldpos;
+                    currentEvent.Use();
+                }
+                if (currentEvent.keyCode == KeyCode.D)
+                {
+                    //set the end position
+                    UnityEngine.MonoBehaviour.print("Setting Track's End Position");
+                    Vector3 screenPosition = Event.current.mousePosition;
+                    screenPosition.y = Camera.current.pixelHeight - screenPosition.y;
+                    Vector3 worldpos = Camera.current.ScreenToWorldPoint(screenPosition);
+                    worldpos.z = 0;
+                    //UnityEngine.MonoBehaviour.print("Mouse pos: " + screenPosition);
                     moveScript.endPosition = worldpos;
                     currentEvent.Use();
+                }
+                if(currentEvent.keyCode == KeyCode.S)
+                {
+                    //move the track and the object
+                    UnityEngine.MonoBehaviour.print("Setting Obstacle's Position");
+                    Vector3 screenPosition = Event.current.mousePosition;
+                    screenPosition.y = Camera.current.pixelHeight - screenPosition.y;
+                    Vector3 worldpos = Camera.current.ScreenToWorldPoint(screenPosition);
+                    worldpos.z = 0;
+
+                    obstacle.transform.position = worldpos;
+                    //get the length and position of the track, then move that proportionally to the object's movement
+                    float trackLength = Vector3.Distance(moveScript.startPosition, moveScript.endPosition);
+                    
+                    moveScript.startPosition.x = obstacle.transform.position.x - (trackLength / 2);
+                    moveScript.startPosition.y = obstacle.transform.position.y;
+                    moveScript.endPosition.x = obstacle.transform.position.x + (trackLength / 2);
+                    moveScript.endPosition.y = obstacle.transform.position.y;  
+
                 }
                 //currentEvent.Use();
                 break;
