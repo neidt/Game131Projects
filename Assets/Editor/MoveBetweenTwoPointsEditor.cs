df15b05d854cabb3f77e2c107a1c7487ca3bd6b3
Natalie Eidt <neidt@yahoo.com>
2018-05-15 11:18:40 -0500

    5-15 work(rotation and scaling)

--- Assets/Editor/MoveBetweenTwoPointsEditor.cs
+++ Assets/Editor/MoveBetweenTwoPointsEditor.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-[CustomEditor(typeof(MoveBetweenTwoPoints))] [CanEditMultipleObjects]
+using System.Text;
+[CustomEditor(typeof(MoveBetweenTwoPoints))]
+[CanEditMultipleObjects]
 public class MoveBetweenTwoPointsEditor : Editor
 {
     //get the script with the variables needed
     MoveBetweenTwoPoints moveScript;
     Transform obstacle;
+    
     SerializedProperty position;
-
+    Vector3 rotateVec = new Vector3(0, 0, 1);
+    public float hSliderVal = 0.5f;
     //private SerializedProperty startPosition;
     //private SerializedProperty endPosition;
 
@@ -17,6 +21,7 @@ public class MoveBetweenTwoPointsEditor : Editor
     {
         moveScript = ((MonoBehaviour)target).gameObject.GetComponent<MoveBetweenTwoPoints>();
         obstacle = ((MonoBehaviour)target).gameObject.GetComponent<Transform>();
+       
         GUIStyle style = new GUIStyle();
         style.richText = true;
         /*
@@ -25,15 +30,29 @@ public class MoveBetweenTwoPointsEditor : Editor
 
         base.OnInspectorGUI();
 
+        //bounciness editor
+        hSliderVal = GUILayout.HorizontalSlider(hSliderVal, 0.0f, 1.0f);
+        GUILayout.Label("Bounciness");
+        GUILayout.Label(hSliderVal.ToString());
+
+        if (GUILayout.Button("Change Bounciness"))
+        {
+
+            //bounciness = hSliderVal;
+
+        }
+
         //show the start and end positions
-        GUILayout.Label("<color=green>Start Position (hold 'a' to change):</color> " + moveScript.startPosition,style);
-        GUILayout.Label("<color=red>End Position (hold 'd' to change):</color> " + moveScript.endPosition,style);
-        GUILayout.Label("Obstacle Position (hold 's' to change): " + obstacle.transform.position,style);
+        GUILayout.Label("<color=green>Start Position (hold 'a' to change):</color> " + moveScript.startPosition, style);
+        GUILayout.Label("<color=red>End Position (hold 'd' to change):</color> " + moveScript.endPosition, style);
+        GUILayout.Label("Obstacle Position (hold 's' to change): " + obstacle.transform.position, style);
+        GUILayout.Label("Obstacle Scale ('u' and 'j' to change): " + obstacle.transform.localScale);
+        GUILayout.Label("Obstacle Rotation ('y' and 'i' to change): " + obstacle.transform.rotation);
         this.serializedObject.ApplyModifiedProperties();
     }
     void OnEnable()
     {
-        position = this.serializedObject.FindProperty("position");  
+        position = this.serializedObject.FindProperty("position");
     }
 
     private void OnSceneGUI()
@@ -42,7 +61,10 @@ public class MoveBetweenTwoPointsEditor : Editor
         moveScript = ((MonoBehaviour)target).gameObject.GetComponent<MoveBetweenTwoPoints>();
         obstacle = ((MonoBehaviour)target).gameObject.GetComponent<Transform>();
         Event currentEvent = Event.current;
+
         
+
+
         //get position of mouse and set the start point when holding left control
         switch (currentEvent.type)
         {
@@ -71,7 +93,7 @@ public class MoveBetweenTwoPointsEditor : Editor
                     moveScript.endPosition = worldpos;
                     currentEvent.Use();
                 }
-                if(currentEvent.keyCode == KeyCode.S)
+                if (currentEvent.keyCode == KeyCode.S)
                 {
                     //move the track and the object
                     UnityEngine.MonoBehaviour.print("Setting Obstacle's Position");
@@ -83,11 +105,31 @@ public class MoveBetweenTwoPointsEditor : Editor
                     obstacle.transform.position = worldpos;
                     //get the length and position of the track, then move that proportionally to the object's movement
                     float trackLength = Vector3.Distance(moveScript.startPosition, moveScript.endPosition);
-                    
+
                     moveScript.startPosition.x = obstacle.transform.position.x - (trackLength / 2);
                     moveScript.startPosition.y = obstacle.transform.position.y;
                     moveScript.endPosition.x = obstacle.transform.position.x + (trackLength / 2);
-                    moveScript.endPosition.y = obstacle.transform.position.y;  
+                    moveScript.endPosition.y = obstacle.transform.position.y;
+                }
+                if (currentEvent.keyCode == KeyCode.U)
+                {
+                    UnityEngine.MonoBehaviour.print("Scaling up");
+                    obstacle.transform.localScale += Vector3.one;
+                }
+                if (currentEvent.keyCode == KeyCode.J)
+                {
+                    UnityEngine.MonoBehaviour.print("Scaling Down");
+                    obstacle.transform.localScale -= Vector3.one;
+                }
+                if (currentEvent.keyCode == KeyCode.Y)
+                {
+                    UnityEngine.MonoBehaviour.print("rotating left");
+                    obstacle.transform.Rotate(rotateVec);
+                }
+                if (currentEvent.keyCode == KeyCode.I)
+                {
+                    UnityEngine.MonoBehaviour.print("rotating right");
+                    obstacle.transform.Rotate(-rotateVec);
                 }
                 //currentEvent.Use();
                 break;
